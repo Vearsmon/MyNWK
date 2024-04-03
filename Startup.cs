d2b7b5fadc8a307f2793af81b7acc1438f5d24ae
Vearsmon <vearsmon285537@gmail.com>
2024-04-03 19:16:34 +0500

    Заготовлен фундамент приложения на ASP.NET Core

---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyNWK;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllersWithViews()
+            .AddSessionStateTempDataProvider();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+
+        app.UseStaticFiles();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
