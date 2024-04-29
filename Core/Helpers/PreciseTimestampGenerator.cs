3d179b6d1b790d55d027682bc7945be6ef8e78bd
Dmitriy Smirnov <d.smirnov@skbkontur.ru>
2024-04-29 21:51:12 +0500

    Precise

---
+++ Core/Helpers/PreciseTimestampGenerator.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace Core.Helpers;
+
+public static class PreciseTimestampGenerator
+{
+    private static DateTime synchronizationMark;
+    
+    private static readonly Stopwatch Stopwatch;
+    private static readonly TimeSpan ConfidenceWindow = TimeSpan.FromMilliseconds(128);
+    private static readonly object LockObject = new();
+    
+    static PreciseTimestampGenerator()
+    {
+        synchronizationMark = DateTime.UtcNow;
+        Stopwatch = new Stopwatch();
+        
+        Synchronize();
+    }
+
+    public static DateTime Generate()
+    {
+        lock (LockObject)
+        {
+            Synchronize();
+
+            var elapsed = Stopwatch.Elapsed;
+            if (elapsed < TimeSpan.Zero)
+            {
+                return synchronizationMark;
+            }
+
+            return synchronizationMark + elapsed;
+        }
+    }
+
+    private static void Synchronize()
+    {
+        while (Stopwatch.Elapsed > ConfidenceWindow || DateTime.UtcNow - synchronizationMark > ConfidenceWindow)
+        {
+            synchronizationMark = DateTime.UtcNow;
+            Stopwatch.Restart();
+        }
+    }
+}
