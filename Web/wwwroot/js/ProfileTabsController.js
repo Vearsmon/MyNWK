16a983bae72f09da76bc6216859d12e731dbc98a
AltyngulBiktasheva <evgeniachu130801@gmail.com>
2024-05-11 17:22:20 +0500

    Ещё одна кнопочка работает

--- Web/wwwroot/js/ProfileTabsController.js
+++ Web/wwwroot/js/ProfileTabsController.js
@@ -12,17 +12,13 @@ function openPurchases () {
     alert("BUTTON1");
     
     ordersInnerContainer[0].hidden = true;
-    // productsInnerContainer[0].style.opacity = 0;
+    productsInnerContainer[0].style.opacity = true;
     purchasesInnerContainer[0].hidden = false;
 
 
     ordersInnerContainer[0].style.opacity = 0;
-    // productsInnerContainer[0].style.opacity = 0;
+    productsInnerContainer[0].style.opacity = 0;
     purchasesInnerContainer[0].style.opacity = 1;
-
-    // for (let product of products) {
-    //     product.style.opacity = 0;
-    // }
 
     for (let order of orders) {
         order.hidden = true;
@@ -31,32 +27,6 @@ function openPurchases () {
     for (let purchase of purchases) {
         purchase.hidden = false;
     }
-
-    // const name = 'Чипсы';
-    // const seller = 'Литовкин';
-    // const price = '256';
-    // const number = '+79966872294';
-    // const bank = 'Тинькофф';
-
-    // const newDiv = document.createElement("div");
-    // newDiv.innerHTML = `<div class="profile-purchases-container">
-    //                         <div class="profile-purchases">
-    //                             <div class="profile-purchases-name">${name}</div>
-
-    //                             <div class="profile-purchases-content-header">Продавец</div>
-    //                             <div class="profile-purchases-content">${seller}</div>
-
-    //                             <div class="profile-purchases-content-header">Цена</div>
-    //                             <div class="profile-purchases-content">${price} руб</div>
-
-    //                             <div class="profile-purchases-content-header">Реквизиты оплаты</div>
-    //                             <div class="profile-purchases-content">${number}, ${bank}</div>
-    //                         </div>
-
-    //                         <button class="profile-purchases-accept">Подтвердить получение</button>
-    //                     </div>`;
-
-    // innerContainer[0].appendChild(newDiv);
 }
 
 function openProducts () {
@@ -64,10 +34,10 @@ function openProducts () {
 
     purchasesInnerContainer[0].hidden = true;
     ordersInnerContainer[0].hidden = true;
-    // productsInnerContainer[0].style.opacity = 1;
+    productsInnerContainer[0].hidden = false;
 
     ordersInnerContainer[0].style.opacity = 0;
-    // productsInnerContainer[0].style.opacity = 1;
+    productsInnerContainer[0].style.opacity = 1;
     purchasesInnerContainer[0].style.opacity = 0;
 
     for (let order of orders) {
@@ -77,36 +47,26 @@ function openProducts () {
     for (let purchase of purchases) {
         purchase.hidden = true;
     }
-
-    // // for (let product of products) {
-    // //     product.style.opacity = 1;
-    // // }
 }
 
 function openOrders () {
     alert("BUTTON3");
 
     purchasesInnerContainer[0].hidden = true;
-    // // productsInnerContainer[0].style.opacity = 0;
+    productsInnerContainer[0].style.opacity = 0;
     ordersInnerContainer[0].hidden = false;
 
     ordersInnerContainer[0].style.opacity = 1;
-    // productsInnerContainer[0].style.opacity = 0;
+    productsInnerContainer[0].style.opacity = 0;
     purchasesInnerContainer[0].style.opacity = 0;
 
     for (let purchase of purchases) {
         purchase.hidden = true;
     }
 
-    // // for (let product of products) {
-    // //     product.style.opacity = 0;
-    // // }
-
     for (let order of orders) {
         order.hidden = false;
     }
-
-
 }
 
 const container = document.querySelector('.profile-container');
