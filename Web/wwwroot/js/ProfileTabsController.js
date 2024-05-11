70c73fe587924e9cb74f7511709c856ec9cafb37
factor-feerist <erik.blodoex@gmail.com>
2024-05-11 20:24:53 +0500

    Работаю пока над профилем

--- Web/wwwroot/js/ProfileTabsController.js
+++ Web/wwwroot/js/ProfileTabsController.js
@@ -10,63 +10,43 @@
 
 function openPurchases () {
     alert("BUTTON1");
-    
-    ordersInnerContainer[0].hidden = true;
-    productsInnerContainer[0].style.opacity = true;
-    purchasesInnerContainer[0].hidden = false;
-
-
-    ordersInnerContainer[0].style.opacity = 0;
-    productsInnerContainer[0].style.opacity = 0;
-    purchasesInnerContainer[0].style.opacity = 1;
-
-    for (let order of orders) {
-        order.hidden = true;
-    }
-
-    for (let purchase of purchases) {
-        purchase.hidden = false;
-    }
 }
 
 function openProducts () {
     alert("BUTTON2");
 
-    purchasesInnerContainer[0].hidden = true;
-    ordersInnerContainer[0].hidden = true;
-    productsInnerContainer[0].hidden = false;
+    const getProductsByUser = new URL('http://127.0.0.1:80/products/get/byUser');
+    const params = {pageNumber:0, batchSize:20};
+    getProductsByUser.search = new URLSearchParams(params).toString();
+    const slots = document.getElementsByClassName("profile-inner-container")[0];
+    fetch(getProductsByUser, {method: 'get'})
+        .then((response) => response.json())
+        .then((products) => {
+            for (product of products) {
+                const productImage = document.createElement('img');
+                productImage.setAttribute('src', product.imageRef);
+                productImage.setAttribute('class', 'profile-inner-container-slot-photo');
 
-    ordersInnerContainer[0].style.opacity = 0;
-    productsInnerContainer[0].style.opacity = 1;
-    purchasesInnerContainer[0].style.opacity = 0;
+                const productPrice = document.createElement('p');
+                productPrice.innerText = `${product['price']} р.`;
 
-    for (let order of orders) {
-        order.hidden = true;
-    }
+                const productInfo = document.createElement('div');
+                productInfo.setAttribute('class', 'profile-inner-container-slot-info');
+                productInfo.textContent = product['title'];
+                productInfo.appendChild(productPrice);
 
-    for (let purchase of purchases) {
-        purchase.hidden = true;
-    }
+                const productSlot = document.createElement('div');
+                productSlot.setAttribute('class', 'profile-inner-container-slot');
+                productSlot.appendChild(productImage);
+                productSlot.appendChild(productInfo);
+
+               slots.appendChild(productSlot);
+            }
+        });
 }
 
 function openOrders () {
     alert("BUTTON3");
-
-    purchasesInnerContainer[0].hidden = true;
-    productsInnerContainer[0].style.opacity = 0;
-    ordersInnerContainer[0].hidden = false;
-
-    ordersInnerContainer[0].style.opacity = 1;
-    productsInnerContainer[0].style.opacity = 0;
-    purchasesInnerContainer[0].style.opacity = 0;
-
-    for (let purchase of purchases) {
-        purchase.hidden = true;
-    }
-
-    for (let order of orders) {
-        order.hidden = false;
-    }
 }
 
 const container = document.querySelector('.profile-container');
@@ -92,5 +72,7 @@ const productsInnerContainer = document.getElementsByClassName("profile-products
 const ordersInnerContainer = document.getElementsByClassName("profile-orders-inner-container");
 
 const purchases = document.getElementsByClassName("profile-purchases-container")
-const products = document.getElementsByClassName("profile-products")
+
 const orders = document.getElementsByClassName("profile-orders-container")
+
+openPurchases();
