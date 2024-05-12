1e85467cd0c540757323ad534268e0d677ac59ea
Dmitriy Smirnov <d.smirnov@skbkontur.ru>
2024-05-12 19:09:40 +0500

    Настройка магаза

--- Core/Services/Markets/IMarketsService.cs
+++ Core/Services/Markets/IMarketsService.cs
@@ -1,6 +1,12 @@
-﻿namespace Core.Services.Markets;
+﻿using Web.Models;
+
+namespace Core.Services.Markets;
 
 public interface IMarketsService
 {
     public Task<List<MarketDto>> GetAllMarkets(RequestContext requestContext);
+    
+    public Task<MarketInfoDto?> GetMarketInfo(RequestContext requestContext, int marketId);
+
+    public Task UpdateAsync(RequestContext requestContext, MarketToUpdate marketToUpdate);
 }
---
+++ Core/Services/Markets/MarketToUpdate.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models;
+
+public class MarketToUpdate
+{
+    [Required]
+    public int Id { get; set; }
+    
+    [StringLength(128)]
+    [DataType(DataType.Text)]
+    public string Name { get; set; }
+    
+    public bool Closed { get; set; }
+    
+    [StringLength(512)]
+    [DataType(DataType.MultilineText)]
+    public string? Description { get; set; }
+    
+    [DataType(DataType.Time)]
+    public TimeOnly? WorksFrom { get; set; }
+
+    [DataType(DataType.Time)]
+    public TimeOnly? WorksTo { get; set; }
+    
+    public bool AutoHide { get; set; }
+}
