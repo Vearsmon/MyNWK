a21f081504956d4f7c04e7c536edd6ab4e86133e
AltyngulBiktasheva <evgeniachu130801@gmail.com>
2024-05-12 19:51:19 +0500

    Добавила переменные цветов для инверсии и кодик

---
+++ Web/wwwroot/js/InversiaController.js
@@ -0,0 +1,39 @@
+function addStyleSheets(href) {
+    const head = document.head;
+    const link = document.createElement('link');
+
+    link.rel = 'stylesheet';
+    link.id = "inversia-true";
+    link.href = href;
+
+    head.appendChild(link);
+    
+    mode = true;
+}
+
+
+function disableStyleSheets(href) {
+    const head = document.head;
+    const link = document.getElementById("inversia-true");
+
+    head.removeChild(link);
+
+    mode=false;
+}
+
+
+function switchMode(href) {
+    if (mode == true) {
+        disableStyleSheets(href);
+    }
+    else {
+        addStyleSheets(href);
+    }
+}
+
+
+
+const modeButton = document.getElementsByClassName("modeButton");
+modeButton[0].addEventListener('click', () => switchMode("~/css/ColorsInversia.css"));
+
+let mode = false;
